eabb868a36e4586687f170a703ac37aea581db07
tck-cy <99850439+tck-cy@users.noreply.github.com>
2022-12-30 10:09:11 -0800

    Basically done efficiency later

--- Roman-Numeral-Converter.js
+++ Roman-Numeral-Converter.js
@@ -1,13 +1,50 @@
 function convertToRoman(num) {
     let result = "";
-    let throusands = Math.floor(num / 1000);
-    let hundreds = Math.floor(num / 100);
-    let tens = Math.floor(num / 10);
-    let ones = num % 10;
-    function splitNumber(num) {}
-    let ten = "X"; let one = "I"; let five = "V"; let fifty = "L"; let hundred = "C"; let fivehundred = "D";
+let ones = num % 10;
+num = Math.floor(num / 10);
+let tens = num % 10;
+num = Math.floor(num / 10);
+let hundreds = num % 10;
+num = Math.floor(num / 10);
+let thousands = num % 10;
+let array = [ones, tens, hundreds, thousands]
+  let ten = "X"; let one = "I"; let five = "V"; let fifty = "L"; let hundred = "C"; let fivehundred = "D";
     let thousand = "M";
-    console.log(tens);
+    //thousands below
+    if (thousands > 0) {
+        if (thousands < 4) {
+            for (let p = 0; p < thousands; p++) {
+                result += thousand;
+            }
+        }
+    }
+    //hundreds below 
+    if (hundreds > 0) {
+        if (hundreds < 4) {
+            for (let m = 0; m < hundreds; m++) {
+                result += hundred;
+            }
+        }
+        if (hundreds > 3 && hundreds < 5) {
+            result += hundred;
+            result += fivehundred;
+        }
+        if (hundreds > 4 && hundreds < 6) {
+            result += fivehundred;
+        }
+        if (hundreds > 5 && hundreds < 9) {
+            result += fivehundred;
+            let newHundreds = hundreds - 5;
+            for (let n = 0; n < newHundreds; n++) {
+                result += hundred;
+            }
+        }
+        if (hundreds > 8 && hundreds < 10) {
+            result += hundred;
+            result += thousand;
+        }
+    }
+    //tens below
     if (tens < 100) {
         if (tens < 4) {
          
@@ -34,8 +71,35 @@ function convertToRoman(num) {
             result += hundred;
         }
     }
-    console.log(result)
+    //ones below
+    if (ones > 0) {
+       if (ones < 100) {
+            if (ones < 4) {
+                for (let j = 0; j < ones; j++) {
+                   result += one;
+                }
+            }
+            if (ones > 3 && ones < 5) {
+                result += one;
+                result += five;
+            }
+            if (ones >4 && ones < 6) {
+                result += five;
+            }
+            if (ones > 5 && ones < 9) {
+                result += five;
+                let newOnes = ones - 5;
+                for (let k = 0; k < newOnes; k++) {
+                    result += one;
+                }
+            }
+            if (ones > 8 && ones < 10) {
+                result += one;
+                result += ten;
+            }
+        }
+    } 
     return result;
    }
    
-   console.log(convertToRoman(36));
+   console.log(convertToRoman(1023));
